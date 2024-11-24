45800e175f210a6d63841efe29f8a5d3163522bc
random-person-on-internet <vedlakkad05@gmail.com>
2024-11-25 01:57:49 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Container from "./container/Container.jsx";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutButton from "./Header/LogoutButton.jsx";
+import Loader from "./Loader/Loader.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
+import Login from "./Login.jsx";
+import Logo from "./Logo.jsx";
+import PostCard from "./PostCard.jsx";
+import Select from "./Select.jsx";
+import Signup from "./Signup.jsx";
+import Protected from "./AuthLayout.jsx";
+import RTE from "./RTE.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+
+export {
+  Input,
+  Login,
+  PostCard,
+  Select,
+  Button,
+  Footer,
+  Header,
+  Loader,
+  Container,
+  Logo,
+  LogoutButton,
+  Signup,
+  Protected,
+  RTE,
+  PostForm,
+};
